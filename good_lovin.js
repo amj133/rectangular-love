b13426c16002e5e7b1bf51de1e3b11f3170a96c7
amj133 <amj@vt.edu>
2018-05-10 10:14:40 -0600

    add condition to love overlap - spec passing

--- good_lovin.js
+++ good_lovin.js
@@ -3,13 +3,16 @@ const LoveRectangle = require('./love_rectangle')
 class GoodLovin {
 
   static loveOverlap(rec1, rec2) {
-    if(rec2.startX > rec1.startX && rec2.startY > rec1.startY) {
+    if(rec1.yLimit <= rec2.startY) {
+      return "No Lovin' :/";
+    }
+    if(rec2.startX >= rec1.startX && rec2.startY >= rec1.startY) {
       var startX = rec2.startX;
       var startY = rec2.startY;
       var newWidth = rec1.xLimit - rec2.startX;
       var newHeight = rec1.yLimit - rec2.startY;
     }
-    if(rec2.startX < rec1.startX && rec2.startY > rec1.startY) {
+    if(rec2.startX <= rec1.startX && rec2.startY >= rec1.startY) {
       var startX = rec1.startX;
       var startY = rec2.startY;
       var newWidth = rec2.xLimit - rec1.startX;

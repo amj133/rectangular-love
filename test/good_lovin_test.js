eac8e092d7377127c250adcf21dd8cc8eac5e3e8
amj133 <amj@vt.edu>
2018-05-10 10:05:24 -0600

    add loverOverlap condition rec2 x < rec1 x && rec2 y > rec1 y

--- test/good_lovin_test.js
+++ test/good_lovin_test.js
@@ -4,7 +4,7 @@ const GoodLovin = require("../good_lovin")
 
 describe("class methods", function() {
   describe("#loveOverlap", function(){
-    it("returns overlapping love rectangle", function() {
+    it("returns overlapping when rec2 x > rec1 x && rec2 y > rec1 y", function() {
       const bob = new LoveRectangle(2, 1, 4, 3)
       const sally = new LoveRectangle(4, 2, 5, 5)
 
@@ -14,6 +14,18 @@ describe("class methods", function() {
       assert.equal(overlap.startY, 2);
       assert.equal(overlap.width, 2);
       assert.equal(overlap.height, 2);
+    });
+
+    it("returns overlapping when rec2 < rec1 x && rec2 y < rec1 y", function() {
+      const bob = new LoveRectangle(3, 2, 5, 3)
+      const sally = new LoveRectangle(2, 4, 4, 6)
+
+      const overlap = GoodLovin.loveOverlap(bob, sally);
+
+      assert.equal(overlap.startX, 3);
+      assert.equal(overlap.startY, 4);
+      assert.equal(overlap.width, 3);
+      assert.equal(overlap.height, 1);
     });
   })
 });
